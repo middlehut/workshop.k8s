0fc9dfbb4494c24d18a3b52828e15b93de889a94
Lyuben Penkovski <penkovski@gmail.com>
2018-10-06 11:44:42 +0300

    2 simple http servers

--- cmd/workshop.k8s/main.go
+++ cmd/workshop.k8s/main.go
@@ -3,13 +3,33 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"fmt"
+
+	"github.com/gorilla/mux"
+	"github.com/middlehut/workshop.k8s/internal/diagnostics"
 )
 
 func main() {
 	log.Println("hello")
 
-	err := http.ListenAndServe(":8080", nil)
+	router := mux.NewRouter()
+	router.HandleFunc("/", hello)
+
+	go func() {
+		err := http.ListenAndServe(":8080", router)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	diags := diagnostics.NewDiagnostics()
+	err := http.ListenAndServe(":8585", diags)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, http.StatusText(http.StatusOK))
 }
