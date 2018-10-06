9123bf1e6314a2fd1c32c4aac6e78ddfa10e6848
Lyuben Penkovski <penkovski@gmail.com>
2018-10-06 12:14:47 +0300

    Configurable server ports and logging

--- cmd/workshop.k8s/main.go
+++ cmd/workshop.k8s/main.go
@@ -6,30 +6,45 @@ import (
 
 	"fmt"
 
+	"os"
+
 	"github.com/gorilla/mux"
 	"github.com/middlehut/workshop.k8s/internal/diagnostics"
 )
 
 func main() {
-	log.Println("hello")
+	log.Println("starting server...")
+
+	blport := os.Getenv("PORT")
+	if len(blport) == 0 {
+		log.Fatal("PORT is missing")
+	}
+
+	diagPort := os.Getenv("DIAG_PORT")
+	if len(diagPort) == 0 {
+		log.Fatal("DIAG_PORT is missing")
+	}
 
 	router := mux.NewRouter()
 	router.HandleFunc("/", hello)
 
 	go func() {
-		err := http.ListenAndServe(":8080", router)
+		log.Println("Application server is preparing to handle connections...")
+		err := http.ListenAndServe(":"+blport, router)
 		if err != nil {
 			log.Fatal(err)
 		}
 	}()
 
+	log.Println("Diagnostics server is preparing to handle connections...")
 	diags := diagnostics.NewDiagnostics()
-	err := http.ListenAndServe(":8585", diags)
+	err := http.ListenAndServe(":"+diagPort, diags)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
+	log.Println("hello handler was called")
 	fmt.Fprint(w, http.StatusText(http.StatusOK))
 }
